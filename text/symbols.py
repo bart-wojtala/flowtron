222ec43305d4c24e534e03fd4668f3880ad85c7f
bart3s <bartlomiej.wojtala@gmail.com>
2022-06-25 21:26:25 +0200

    Handle polish symbols

--- text/symbols.py
+++ text/symbols.py
@@ -7,14 +7,19 @@ The default is a set of ASCII characters that works well for English or text tha
 from text import cmudict
 
 _punctuation = '!\'",.:;? '
-_math = '#%&*+-/[]()'
-_special = '_@©°½—₩€$'
-_accented = 'áçéêëñöøćž'
-_numbers = '0123456789'
-_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+# _math = '#%&*+-/[]()'
+# _special = '_@©°½—₩€$'
+# _accented = 'áçéêëñöøćž'
+# _numbers = '0123456789'
+# _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+_pad        = '_'
+_special = '-'
+_letters = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻaąbcćdeęfghijklłmnńoóprsśtuwyzźż'
 
 # Prepend "@" to ARPAbet symbols to ensure uniqueness (some are the same as uppercase letters):
 _arpabet = ['@' + s for s in cmudict.valid_symbols]
 
 # Export all symbols:
-symbols = list(_punctuation + _math + _special + _accented + _numbers + _letters) + _arpabet
+# symbols = list(_punctuation + _math + _special + _accented + _numbers + _letters) + _arpabet
+symbols = [_pad] + list(_special) + list(_punctuation) + list(_letters) + _arpabet
